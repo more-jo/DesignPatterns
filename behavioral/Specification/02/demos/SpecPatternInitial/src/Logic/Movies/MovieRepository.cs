d6b0a2a453e3be401f65911f13f1082625601652
Moor <j.moor@stratec.com>
2023-11-01 15:49:12 +0100

    Added Specification pattern

---
+++ behavioral/Specification/02/demos/SpecPatternInitial/src/Logic/Movies/MovieRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CSharpFunctionalExtensions;
+using Logic.Utils;
+using NHibernate;
+using NHibernate.Linq;
+
+namespace Logic.Movies
+{
+    public class MovieRepository
+    {
+        public Maybe<Movie> GetOne(long id)
+        {
+            using (ISession session = SessionFactory.OpenSession())
+            {
+                return session.Get<Movie>(id);
+            }
+        }
+
+        public IReadOnlyList<Movie> GetList()
+        {
+            using (ISession session = SessionFactory.OpenSession())
+            {
+                return session.Query<Movie>().ToList();
+            }
+        }
+    }
+}
---
+++ behavioral/Specification/02/demos/SpecPatternInitial/src/UI/Movies/MovieListViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Logic.Movies;
+using UI.Common;
+
+namespace UI.Movies
+{
+    public class MovieListViewModel : ViewModel
+    {
+        private readonly MovieRepository _repository;
+
+        public Command SearchCommand { get; }
+        public Command<long> BuyTicketCommand { get; }
+        public IReadOnlyList<Movie> Movies { get; private set; }
+
+        public MovieListViewModel()
+        {
+            _repository = new MovieRepository();
+
+            SearchCommand = new Command(Search);
+            BuyTicketCommand = new Command<long>(BuyTicket);
+        }
+
+        private void BuyTicket(long movieId)
+        {
+            MessageBox.Show("You've bought a ticket", "Success",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Search()
+        {
+            Movies = _repository.GetList();
+            Notify(nameof(Movies));
+        }
+    }
+}
